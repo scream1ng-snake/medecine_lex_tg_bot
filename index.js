58a3a76c4058c2a735bf21ea8b660915c6748935
lykasov_am <lykasov_am@lexema.ru>
2023-05-21 18:10:52 +0500

    UPD: app token

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const token = '6070228695:AAEND6FJmusuX-c3g6rujIWhRv8PbFpfQ5Y';
 
 const bot = new TelegramBot(token, {polling: true});
 
-const webAppUrl = 'https://646912b9268c134b25d04d08--curious-puffpuff-a70a97.netlify.app/';
+const webAppUrl = 'https://curious-puffpuff-a70a97.netlify.app';
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
   if(text === '/start') {
+    await bot.sendMessage(chatId, 'Ниже появиться кнопка, заполни форму', {
+      reply_markup: {
+        keyboard: [
+          [{
+            text: 'Заполни форму',
+            web_app: {
+              url: webAppUrl + '/form'
+            }
+          }]
+        ]
+      }
+    });
     await bot.sendMessage(chatId, 'Интерет магазин', {
       reply_markup: {
         inline_keyboard: [
