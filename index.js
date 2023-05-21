46d7f63a1296d6280960d8ea22e226974575809a
lykasov_am <lykasov_am@lexema.ru>
2023-05-21 20:49:25 +0500

    ADD: express web server

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const cors = require('cors');
 
+const PORT = 8000;
 const token = '6070228695:AAEND6FJmusuX-c3g6rujIWhRv8PbFpfQ5Y';
 
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(
+  token, 
+  { polling: true }
+);
+
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 const webAppUrl = 'https://curious-puffpuff-a70a97.netlify.app';
 
@@ -45,7 +55,37 @@ bot.on('message', async (msg) => {
         bot.sendMessage(chatId, 'Всю информацию вы получите в нашем чате');
       }, 3000);
     } catch (e) {
-      console.log(e)
+      console.error(e)
     }
   }
 });
+
+app.post('/web-data', async (req, res) => {
+  const {queryId, products, totalPrice} = req.body;
+  try {
+    await bot.answerWebAppQuery(queryId, {
+      type: 'article',
+      id: queryId,
+      title: 'Успешная покупка!',
+      input_message_content: {
+        message_text: 'Поздравляю с покупкой! Вы приобрели товар на сумму: ' + totalPrice
+      }
+    })
+    return res.status(200).json({});
+  } catch (e) {
+    console.error(e);
+    await bot.answerWebAppQuery(queryId, {
+      type: 'article',
+      id: queryId,
+      title: 'Не удалось приобрести товар(((',
+      input_message_content: {
+        message_text: 'Не удалось приобрести товар((('
+      }
+    })
+    return res.status(500).json({});
+  }
+})
+
+app.listen(PORT, () => 
+  console.log('Server running at port = ' + PORT)
+)
